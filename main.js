b3388bcd154945204158fe31e35d98d46e87753d
cristianllambro <llambro.cristian1996@gmail.comm>
2022-02-22 16:24:48 +0100

    complete work

--- main.js
+++ main.js
@@ -1,1 +1,13 @@
-let 
+let iconHamb = document.querySelector('.fa-bars')
+let menuHamb = document.querySelector('.hamburger-menu');
+let iconTime = document.querySelector('.fa-times')
+
+iconHamb.addEventListener('click', function() {
+    menuHamb.classList.add('active')
+})
+
+iconTime.addEventListener('click', function() {
+    menuHamb.classList.remove('active')
+})
+
+
